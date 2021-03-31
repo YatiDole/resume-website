e8c4c939e3a4cb24559708ce5d7cfe6e38f4bab5
YatiDole <ydole@hawk.iit.edu>
2021-03-31 17:16:04 -0500

    Changes to js script

--- visitor_counter.js
+++ visitor_counter.js
@@ -1,26 +1,5 @@
-window.onload = () => {
-    const getVisitCount = async (website) => {
-      let url = "https://lcu3dxdmo1.execute-api.us-east-2.amazonaws.com/Prod";
-      const response = await fetch(`${url}/visit_count`, {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          Website: website,
-        }),
-      });
-      return response.json();
-    };
-  
-    getVisitCount("Cloud Resume Challenge")
-      .then((res) => {
-        document.getElementById("counter").innerHTML = String(res.count).padStart(
-          6,
-          "0"
-        );
-      })
-      .catch((e) => {
-        console.log(e);
-      }
+fetch('https://04395o5vvh.execute-api.us-east-1.amazonaws.com/Dev')
+.then((response) => response.text())
+.then((data) => {
+  document.getElementById("number").innerHTML = data;
+})
