eb1188056d621a12398ebac83e436c183a2975c1
YatiDole <ydole@hawk.iit.edu>
2021-03-31 14:58:07 -0500

    pdf file

--- visitor_counter.js
+++ visitor_counter.js
@@ -1,12 +1,26 @@
-const apiUrl = 'https://emil3mqc5f.execute-api.eu-central-1.amazonaws.com/prod/counter'
-const countElement = document.getElementById('count');
-
-updateVisitCount();
-
-function updateVisitCount() {
-    fetch(apiUrl)
-        .then(res => res.json())
-        .then(res => {
-        countElement.innerHTML = res.visits;
-    });
-}
+window.onload = () => {
+    const getVisitCount = async (website) => {
+      let url = "https://lcu3dxdmo1.execute-api.us-east-2.amazonaws.com/Prod";
+      const response = await fetch(`${url}/visit_count`, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          Website: website,
+        }),
+      });
+      return response.json();
+    };
+  
+    getVisitCount("Cloud Resume Challenge")
+      .then((res) => {
+        document.getElementById("counter").innerHTML = String(res.count).padStart(
+          6,
+          "0"
+        );
+      })
+      .catch((e) => {
+        console.log(e);
+      }
