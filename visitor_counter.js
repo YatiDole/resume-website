4cf7ecb8ae63454d0b03e30359372ece0e9aa50a
YatiDole <ydole@hawk.iit.edu>
2021-04-01 00:54:12 -0500

    updating js counter api

--- visitor_counter.js
+++ visitor_counter.js
@@ -1,4 +1,4 @@
-fetch('https://ih7vyih0t5.execute-api.us-east-1.amazonaws.com/Prod')
+fetch('https://${ServerlessRestApi}.execute-api.us-east-1.amazonaws.com/Prod/count')
 .then((response) => response.text())
 .then((data) => {
   document.getElementById("number").innerHTML = data;
